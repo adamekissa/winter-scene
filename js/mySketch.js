04375965a5bce90109017a8eaf64bec4ba243a2b
Mohammed Adam <adamekissa@gmail.com>
2024-05-30 16:12:59 +0100

    SnowMen

--- js/mySketch.js
+++ js/mySketch.js
@@ -16,6 +16,9 @@ function draw() {
     const x = random(0, width);
     const y = random(0, height);
     circle(x, y, 10);
+
+    drawSnowMan(200, 355) ;
+    drawSnowMan(400, 355) ;
 }
 
 function mouseClicked() {
@@ -27,4 +30,40 @@ function keyPressed() {
     if (key === "s") {
         save("my-p5-screenshot");
     }
+}
+
+
+function drawSnowMan(x, y) {
+
+	let xCentre = x;
+	let yCentre = y;
+
+	//Head
+	fill("white")
+	circle(xCentre + 0, yCentre + 0, 100);
+
+	//Eyes
+	fill("#353b48")
+	circle(xCentre - 20, yCentre - 15, 10);
+	circle(xCentre + 20, yCentre - 15, 10);
+
+	//Nose
+	fill("black")
+	circle(xCentre + 0, yCentre + 15, 30);
+
+	//Body
+	fill("white")
+	circle(xCentre + 0, yCentre + 135, 170);
+	fill(255, 255, 255);
+
+	//Belly-button
+	fill("blue")
+	circle(xCentre + 0, yCentre + 115, 10);
+	circle(xCentre + 0, yCentre + 135, 10);
+	circle(xCentre + 0, yCentre + 155, 10);
+
+	//Scarf
+	fill("red");
+	rectMode(CENTER)
+	rect(xCentre - 20, yCentre + 50, 120, 15, 15);
 }
